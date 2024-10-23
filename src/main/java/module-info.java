25f5fc107316d5214a04cbe6a12e4914f8950b0a
Wiktor Baranowicz <s095866@student.tu.kielce.pl>
2024-10-23 16:04:44 +0200

    jackson

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,4 +2,6 @@ module com.example.javaonlineproject {
     requires javafx.controls;
     requires java.desktop;
     exports com.example.javaonlineproject;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.core;
 }
